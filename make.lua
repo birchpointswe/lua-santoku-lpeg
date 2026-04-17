6afc4c855edda781a2f33a1ced8cd3b752dd92e9
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-16 22:49:37 -0400

    bump version to 0.0.2-1

--- make.lua
+++ make.lua
@@ -1,11 +1,11 @@
 local env = {
   name = "santoku-lpeg",
-  version = "0.0.1-1",
+  version = "0.0.2-1",
   variable_prefix = "TK_LPEG",
   license = "MIT",
   public = true,
   dependencies = {
-    "lua >= 5.1",
+    "lua == 5.1",
     "lpeg >= 1.1.0-2",
   },
   test = {
@@ -15,7 +15,7 @@ local env = {
   }
 }
 
-env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.homepage = "https://github.com/birchpointswe/lua-" .. env.name
 env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
